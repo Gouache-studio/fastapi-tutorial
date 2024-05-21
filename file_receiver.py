1b7a1ed90a5dc3375c5b10ec62f4b29623634525
gromit <gromit@github.com>
2024-05-21 14:30:15 +0900

    filereceiver

--- file_receiver.py
+++ file_receiver.py
@@ -1,30 +1,44 @@
 import os
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import FileResponse
+from typing import List, Optional
 
 app = FastAPI()
 
 # 파일을 저장할 경로 지정
-# SAVE_DIR = "/Users/mac/Desktop/Gromit/_02_STUDY/_05_SideProject/FastAPI/NAS/"
-script_dir = os.path.dirname(os.path.abspath(__file__))
-# NAS 폴더의 상위 폴더로 이동한 상대 경로
-relative_dir = os.path.join("..", "NAS")
-# 상대 경로를 절대 경로로 변환하여 저장 경로 설정
-SAVE_DIR = os.path.abspath(os.path.join(script_dir, relative_dir))
+SAVE_DIR = "/Users/mac/Desktop/Gromit/_02_STUDY/_05_SideProject/FastAPI/NAS/"
 
 @app.post("/upload")
-
-async def upload_file(file: UploadFile = File(...)):
-    print(file.filename)
-    # 파일을 읽어서 저장할 경로 지정
-    save_path = os.path.join(SAVE_DIR, file.filename)
+async def upload_files(
+    tracks : Optional[List[UploadFile]] = File(...), 
+    album_covers : Optional[List[UploadFile]] = File(...), 
+    album_booklets  : Optional[List[UploadFile]] = File(...)):
     
-    # 파일을 저장
-    with open(save_path, "wb") as f:
-        contents = await file.read()
-        f.write(contents)
+    response_data = {"tracks": [], "album_cover": [], "album_booklets": []}
+    success = True
     
-    return {"filename": file.filename, "save_path": save_path}
-
+    # 음원 파일 처리 로직
+    if tracks is not None:
+        for track in tracks:
+            save_path = os.path.join(SAVE_DIR, track.filename)
+            with open(save_path, "wb") as f:
+                contents = await track.read()
+                f.write(contents)
+            response_data["tracks"].append({"filename": track.filename, "save_path": save_path})
+    
+    # 이미지 파일 처리 로직
+    if album_covers is not None:
+        for album_cover in album_covers :
+            pass
+    
+    # 텍스트 파일 처리 로직
+    if album_booklets is not None:
+        for album_booklet in album_booklets:
+            pass
+    
+    if success:
+        return {"message": "Files uploaded successfully", **response_data}
+    else:
+        return {"message": "Failed to upload files", "error": "Reason for failure"}
 
 
